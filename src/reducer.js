33ba7572790db2bf57dde337c1b063038d1f656b
Matt Lewis <domattthings@gmail.com>
2017-01-31 18:28:08 +0000

    Added config for marked

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,15 @@
 import marked from 'marked';
 import { HANDLE_INPUT_CHANGE } from './actions';
 
+const markedConfig = {
+  breaks: true,
+};
+
 const markdownReducer = (state = { compiledMarkDown: '' }, action) => {
   switch (action.type) {
     case HANDLE_INPUT_CHANGE:
       return Object.assign({}, state, {
-        compiledMarkDown: marked(action.input),
+        compiledMarkDown: marked(action.input, markedConfig),
       });
     default:
       return state;
